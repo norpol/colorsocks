800917713fdcb3c4d594b913ac9dd0bebf30619a
norpol <norpol@woerm.at>
2015-03-29 18:36:48 +0200

    updates dom, no more canvas, receiving websockets broadcast messages is working now

--- public/client.js
+++ public/client.js
@@ -1,30 +1,24 @@
 //Bootstrapping...
 var socket = new WebSocket("ws://localhost:8080", "echo-protocol");
-
+console.log("Foo early");
 socket.onerror = function() {
    console.error("Connection Error");
 };
 
 socket.onopen = function() {
    console.log((new Date()) + "WebSocket Client Connected");
-
-   function sendNumber() {
-      if (socket.readyState === socket.OPEN) {
-         var number = Math.round(Math.random() * 0xFFFFFF);
-         socket.send(number.toString());
-         setTimeout(sendNumber, 1000);
-      }
-   }
-   sendNumber();
+   socket.send("Häcking UTF-8 ↓");
 };
 
 socket.onclose = function() {
    console.log("echo-protocol Client Closed");
 };
 
-socket.onmessage = function(e) {
-   if (typeof e.data === "string") {
-      console.log("Received: '" + e.data + "'");
-   }
+
+socket.onmessage = function (event) {
+   console.log(event.data);
+   var background = document.getElementById("background");
+   var wsColor = event.data;
+   background.style.backgroundColor = wsColor;
 };
 
