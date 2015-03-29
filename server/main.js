8f9e14c952e5dfb4ce4f871904c4379a2f453858
norpol <norpol@woerm.at>
2015-03-29 07:32:10 +0200

    receive utf8 working

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@
 // for dependencies see also package.json
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+var clientConnections = [];
 
 var server = http.createServer(function(request, response) {
    console.log((new Date()) + ' Received request for ' + request.url);
@@ -23,24 +24,15 @@ wsServer = new WebSocketServer({
    autoAcceptConnections: false
 });
 
-function originIsAllowed(origin) {
-   return true;
-}
-
 wsServer.on('request', function(request) {
-   if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-   }
-
    var connection = request.accept('echo-protocol', request.origin);
-   console.log((new Date()) + ' Connection accepted.');
+   clientConnections.push(connection);
+   console.log((new Date()) + ' Connection accepted by ' +  connection.remoteAddress);
    connection.on('message', function(message) {
       if (message.type === 'utf8') {
-         console.log('Received Message: ' + message.binaryData.length + ' bytes');
-         connection.sendBytes(message.binaryData);
+         console.log(message);
+         console.log('Received Message: ' + message.utf8Data.length + ' bytes');
+         connection.sendUTF(message.utf8Data);
       }
    });
    connection.on('close', function(reasonCode, description) {
