5c899a195201782e988aa711882521a2762a938b
norpol <norpol@woerm.at>
2015-03-29 06:33:00 +0200

    newline

--- public/client.js
+++ public/client.js
@@ -1,6 +1,30 @@
 //Bootstrapping...
-//var wsServer = new WebSocket("ws://localhost:8088");
-var canvas = document.getElementById("mainCanvas");
-var context = canvas.getContext('2d');
-context.fillStyle = 'red';
-context.fillRect(30, 30, 50, 50);
+var client = new WebSocket("ws://localhost:8080", "echo-protocol");
+
+client.onerror = function() {
+   console.log("Connection Error");
+};
+
+client.onopen = function() {
+   console.log((new Date()) + "WebSocket Client Connected");
+
+   function sendNumber() {
+      if (client.readyState === client.OPEN) {
+         var number = Math.round(Math.random() * 0xFFFFFF);
+         client.send(number.toString());
+         setTimeout(sendNumber, 1000);
+      }
+   }
+   sendNumber();
+};
+
+client.onclose = function() {
+   console.log("echo-protocol Client Closed");
+};
+
+client.onmessage = function(e) {
+   if (typeof e.data === "string") {
+      console.log("Received: '" + e.data + "'");
+   }
+};
+
